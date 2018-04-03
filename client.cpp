369c539cab84b37c58bbbb17e0aafae4b37d8d2b
PierreLP-Epsi <pierre.lepallec@epsi.fr>
2018-04-03 14:33:26 +0200

    Communication Client/Serveur OK

---
+++ client.cpp
@@ -0,0 +1,54 @@
+#include "ClientSocket.h"
+#include "SocketException.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+void send_message(ClientSocket client, string user){
+	string message;
+	string reply;
+	
+	while(true){
+		getline(cin, message);
+		client << user + " : " + message;
+		client >> reply;
+		cout << reply << endl;
+	}
+}
+
+int main (int argc, char* argv[]) {
+  if(argc != 3) {
+    cout << "invalid number of arguments: call with `./client IP port`" << endl;
+    return 1;
+  }
+
+  string host = argv[1];
+  int port = atoi(argv[2]);
+
+  try {
+    ClientSocket client_socket ( host, port );
+    string reply;
+	string user;
+
+	try {
+		client_socket << "Coucou";
+		client_socket >> reply;
+	} catch ( SocketException& ) {}
+	
+    cout << "We received this response from the server:\n\"" << reply << "\"\n";;
+	
+	cout << "Entrer votre pseudo : ";
+	cin >> user;
+	cout << "Votre pseudo est : " << user << endl;
+
+	send_message(client_socket, user);
+  }  
+  
+  catch ( SocketException& e ) {
+    cout << "Exception was caught:" << e.description() << endl;
+  }
+
+  return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,68 @@
+#include "ServerSocket.h"
+#include "SocketException.h"
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <thread>
+
+using namespace std;
+
+int main (int argc, char* argv[]) {
+  if(argc !=2) {
+    cout << "invalid number of arguments: call with `./server port`" << endl;
+    return 1;
+  }
+
+  ServerSocket sockets[3];
+
+  int port = atoi(argv[1]);
+
+  cout << "running....\n";
+
+  try {
+    ServerSocket server ( port );
+	int i = 0;
+    while ( true ) {
+
+      server.accept ( sockets[i] );
+	  i++;
+	  std::thread t1([&](){
+		  try {
+			while ( true ) {
+			  string data;
+			  sockets[0] >> data;
+			  sockets[0] << data;
+			  for(int x = 0; x < sizeof(sockets); x++){
+				  sockets[x] << data;
+			  }
+			  cout << data << endl;
+			}
+		  } catch ( SocketException& ) {}
+	  });
+
+	  std::thread t2([&](){
+		  try {
+			while (true) {
+				if(i>1)
+				{
+				  string data;
+				  sockets[1] >> data;
+
+				  for(int x = 0; x < sizeof(sockets); x++){
+					  sockets[x] << data;
+				  }
+				  cout << data << endl;
+				}
+			}
+		  } catch ( SocketException& ) {}
+	  });
+
+	  t1.join();
+	  t2.join();
+    }
+  } catch ( SocketException& e ) {
+    cout << "Exception was caught:" << e.description() << "\nExiting" << endl;
+  }
+
+  return 0;
+}
